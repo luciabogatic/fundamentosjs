f1f6111929c2f5cea52fe6c339439fa97b39f371
luciabogatic <lbogatic24@ucema.edu.ar>
2022-09-20 13:23:48 -0300

    fundamentos js 1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+
+const nombre = 'Juan'
+const apellido = 'Perez'
+const edad = 18
+
+console.log(edad)
+console.log(edad + 22)
+
+
+
+  return (
+<>
+<h1>const - let - var</h1>
+</>
+);
+}
+
+export default App;
